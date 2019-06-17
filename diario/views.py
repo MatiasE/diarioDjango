371887da2759c21eae32904097ea06447e273ff9
Matias <mepi-11@hotmail.com>
2019-06-17 15:36:37 -0300

    Primer View

--- diario/views.py
+++ diario/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Articulo
+from django.utils import timezone
 
 # Create your views here.
+def pagina_inicio(request):
+    articulos = Articulo.objects.all()
+    return render(request, 'diario/pagina_inicio.html', {'articulos': articulos})
